bc64277d645fb2f0802fa5e64a13c9d516ce7564
Jake Swenson <jake@jakeswenson.com>
2019-07-03 12:33:25 -0700

    protoc fix

--- build/install-protoc.sh
+++ build/install-protoc.sh
@@ -4,7 +4,7 @@ set -e
 if [ ! -d "$HOME/protobuf/lib" ]; then
   wget https://github.com/protocolbuffers/protobuf/releases/download/v3.8.0/protobuf-all-3.8.0.tar.gz
   tar -xzvf protobuf-all-3.8.0.tar.gz
-  cd protobuf-all-3.8.0 && ./configure --prefix=$HOME/protobuf && make && make install
+  cd protobuf-3.8.0 && ./configure --prefix=$HOME/protobuf && make && make install
 else
   echo "Using cached protobuf."
 fi
